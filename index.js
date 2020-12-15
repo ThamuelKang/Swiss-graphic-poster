fc0d0e6268e3ee4546dd5e0cfcd0b5ee1dd81456
ThamuelKang <samuel.june.kang@gmail.com>
2020-12-15 01:01:51 -0800

    added Typography controls and uploaded font files

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+//layout
 const rowValue = document.getElementById('row');
 const rowSlider = document.querySelector(`input[name="row-slider"]`);
 const columnValue = document.getElementById('column');
@@ -5,28 +6,34 @@ const columnSlider = document.querySelector(`input[name="column-slider"]`);
 const addTextbox = document.querySelector(`input[value="Add"]`);
 const removeTextbox = document.querySelector(`input[value="Remove"]`);
 
+//typography
 
-const body = document.querySelector(".body");
+const header = document.querySelector("textarea.header");
+const body = document.querySelectorAll("textarea.body");
+
+const headerSize = document.querySelector(`input[name="header-size"]`);
+const bodySize = document.querySelector(`input[name="body-size"]`);
+
+const headerColor = document.querySelector(`input[name="header-color"]`);
+const bodyColor = document.querySelector(`input[name="body-color"]`);
+
+const headerTypeface = document.querySelector(`select[name="header-typeface"]`);
+const bodyTypeface = document.querySelector(`select[name="body-typeface"]`);
+
+const headerFontWeight = document.querySelector(`select[name="header-font-weight"]`);
+const bodyFontWeight = document.querySelector(`select[name="body-font-weight"]`);
+
 
 const poster = document.querySelector('div.poster');
+
+
+
 
 // row and column slider and value
 
 rowSlider.addEventListener("input", function () {
     rowValue.innerHTML = this.value;
     poster.style.gridTemplateRows = 'repeat' + '(' + this.value + ', 1fr)'
-
-    // if (this.value > 5) {
-    //     for (i = 0; i < 3; i++) {
-    //         console.log(i)
-    //         poster.appendChild(textarea);
-    //     }
-    // } else {
-    //     for (i = 0; i < 3; i++) {
-    //         console.log(i)
-    //         poster.removeChild(textarea);
-    //     }
-    // }
 })
 
 columnSlider.addEventListener("input", function () {
@@ -36,15 +43,64 @@ columnSlider.addEventListener("input", function () {
 
 
 addTextbox.addEventListener("click", function () {
-    console.log("hi")
-    poster.innerHTML += `<textarea name="body" class="body">Lorem ipsum I'm a great designer</textarea>`
+    let textarea = document.createElement("TEXTAREA");
+    poster.appendChild(textarea).classList.add("body");
+
     poster.firstElementChild.classList.add("header");
-    poster.firstElementChild.classList.remove("body")
-    poster.firstElementChild.innerHTML = "Generate a good looking poster!"
-
-
+    poster.firstElementChild.classList.remove("body");
 })
 
 removeTextbox.addEventListener("click", function () {
     poster.removeChild(poster.lastElementChild);
+    body.length--;
+    console.log(body.length)
 })
+
+
+
+// typography control
+
+headerSize.addEventListener("input", function () {
+    header.style.fontSize = this.value + "px";
+})
+
+bodySize.addEventListener("input", function () {
+    for (i = 0; i < body.length; i++) {
+        body[i].style.fontSize = this.value + "px"
+        console.log(body.length)
+    }
+})
+
+headerColor.addEventListener("input", function () {
+    header.style.color = this.value;
+})
+
+bodyColor.addEventListener("input", function () {
+    for (i = 0; i < body.length; i++) {
+        body[i].style.color = this.value;
+    }
+})
+
+// headerFontWeight.addEventListener("change", function () {
+//     header.style.fontWeight = this.value;
+// })
+
+function headerFontWeightChange() {
+    header.style.fontWeight = headerFontWeight.value;
+}
+
+function bodyFontWeightChange() {
+    for (i = 0; i < body.length; i++) {
+        body[i].style.fontWeight = bodyFontWeight.value;
+    }
+}
+
+function headerTypefaceChange() {
+    header.style.fontFamily = headerTypeface.value;
+}
+
+function bodyTypefaceChange() {
+    for (i = 0; i < body.length; i++) {
+        body[i].style.fontFamily = bodyTypeface.value;
+    }
+}
